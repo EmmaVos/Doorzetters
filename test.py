71d421033dc2d7d295ed18409fa0f364796945d5
Anouk <anoukjanssen1@gmail.com>
2021-06-07 14:16:20 +0200

    Collision aangepast

--- test.py
+++ test.py
@@ -122,9 +122,10 @@ class Player:
         self.rect.x += dx
         self.rect.y += dy
 
-        if self.rect.bottom > screen_height:
-            self.rect.bottom = screen_height
-            dy = 0
+        #------- onderstaand is niet nodig. Dit check is al in de collision op y-as
+        # if self.rect.bottom > screen_height:   
+        #     self.rect.bottom = screen_height
+        #     dy = 0
 
         # Tekent speler op het scherm
         screen.blit(self.image, self.rect)
